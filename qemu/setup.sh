5be796d25f085b810c070b613f25f436c1b5ab8a
SametSisartenep <rodrigosloop@gmail.com>
2015-03-26 02:32:13 +0100

    QEMU setup file updated.

--- qemu/setup.sh
+++ qemu/setup.sh
@@ -5,4 +5,7 @@ wget http://wiki.qemu-project.org/download/qemu-2.3.0-rc0.tar.bz2
 tar -xvf *.tar.*
 rm -rfv *.tar.*
 
-cd qemu* && make -j4 && sudo make install
+cd qemu*
+./configure
+make -j4
+sudo make install
