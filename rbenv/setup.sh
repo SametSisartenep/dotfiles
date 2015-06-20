a5f45b6de8d5d1315c998dbe19f031f2e732af05
SametSisartenep <rodrigosloop@gmail.com>
2015-06-20 06:18:24 +0200

    Extended rbenv installation.

--- rbenv/setup.sh
+++ rbenv/setup.sh
@@ -5,3 +5,12 @@ git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+
+rbenv install 2.2.2
+rbenv global 2.2.2
+
+# Now tell Rubygems not to install the documentation for each package
+# locally, and then install bundler
+
+echo "gem: --no-ri --no-rdoc" > $HOME/.gemrc
+gem install bundler
