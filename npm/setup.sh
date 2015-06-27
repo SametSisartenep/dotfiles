05b1c73dba16561078ac906498e9ebf07c664966
SametSisartenep <rodrigosloop@gmail.com>
2015-06-27 03:24:23 +0200

    markdown package removed from NPM configuration.

--- npm/setup.sh
+++ npm/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-NPM_PACKAGES=( jshint node-gyp grunt-cli nodemon markdown )
+NPM_PACKAGES=( jshint node-gyp grunt-cli nodemon )
 
 for PACKAGE in ${NPM_PACKAGES[@]}; do
   sudo npm install -g $PACKAGE
