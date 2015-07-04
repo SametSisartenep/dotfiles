d1e5568d2491006372f08f97c702103e910c4689
SametSisartenep <rodrigosloop@gmail.com>
2015-07-04 16:58:06 +0200

    QEMU version update.

--- qemu/setup.sh
+++ qemu/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget http://wiki.qemu-project.org/download/qemu-2.3.0-rc0.tar.bz2
+wget http://wiki.qemu-project.org/download/qemu-2.3.0.tar.bz2
 
 tar -xvf *.tar.*
 rm -rfv *.tar.*
