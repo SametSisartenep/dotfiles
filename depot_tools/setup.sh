35dd304a6a145643c4b8961f6ca974ccd2225fa6
SametSisartenep <rodrigosloop@gmail.com>
2016-07-14 17:26:52 +0200

    I no longer use this tools.

--- depot_tools/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p $HOME/tools
-
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git $HOME/tools/depot_tools
-
-# make sure you have 'depot_tools' updated
-gclient
